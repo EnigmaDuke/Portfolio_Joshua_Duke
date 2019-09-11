5c316b13154c05b727a38200ee93d07d0b1179d5
Joshua Duke <dukejoshuae@gmail.com>
2019-09-10 20:49:18 -0400

    Update project section to display information on hover

---
+++ script/script.js
@@ -0,0 +1,23 @@
+$("a[href*='#']").click(function(e) {
+  e.preventDefault()
+
+  let position = $($(this).attr('href')).offset().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: position 
+    }, 600, 'linear'
+  );
+});
+
+let bounce = document.querySelector("#bounce");
+
+$("#bounce").click(function(){
+  let position = $(this).offset().top + 150 + "px";
+
+  $("html, body").animate(
+    {
+      scrollTop: position 
+    }, 600, 'linear'
+  );
+})
