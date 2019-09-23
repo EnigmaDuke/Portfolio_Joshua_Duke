e1102466086a9115f79ef5b171277b6844c3f80e
Joshua Duke <dukejoshuae@gmail.com>
2019-09-22 22:00:01 -0400

    Adding form validation

--- script/script.js
+++ script/script.js
@@ -1,4 +1,32 @@
 
+let form = document.querySelector("form");
+let error = document.querySelector("#error");
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+
+//prevent submit from submitting
+$("form").submit(function (e){
+  e.preventDefault();
+
+  var error = "";
+
+  if ($("#name").val() == ""){
+    error += "<p>The name field is required!</p>";
+  }
+
+  if ($("#email").val() == ""){
+    error += "<p>The Email field is required!</p>";
+  }
+
+  if ($("#message").val() == ""){
+    error += "<p>A Message is required!</p>";
+  }
+
+  $("#error").html(error);
+});
+
+
+
 
 
 
